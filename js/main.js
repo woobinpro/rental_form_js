da748893ddb90dbf4242b9aab63aa2cd578ded60
woobinpro <woobinpro@gmail.com>
2023-03-22 02:03:25 +0900

    change some code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
     $('#clear_signature').click(function(){
         signaturePad.clear();
     });
-    $('.dm-uploader').dmUploader({ //
+    $('#front_idcard').dmUploader({ //
         url: 'https://82.165.190.74:3001/api/uploadID',
         fileName:'file',
         maxFileSize: 3000000, // 3 Megs 
@@ -23,8 +23,60 @@ $(function(){
         onUploadSuccess: function(id, data){
           console.log('Server Response for file #' + id + ': ' + JSON.stringify(data));
           console.log('Upload of file #' + id + ' COMPLETED', 'success');
-          $(this).children('img')[0].attr('src','http://82.165.190.74/images/'+data.name);
-          $(this).children('img')[0].show();
+          $('#front_idcard img').attr('src','http://82.165.190.74/images/'+data.name);
+          $('#front_idcard img').show();
+        },
+        onUploadError: function(id, xhr, status, message){
+          console.log('danger' + id + ":" + status+ "-" + message);
+        }
+      });
+      $('#back_idcard').dmUploader({ //
+        url: 'https://82.165.190.74:3001/api/uploadID',
+        fileName:'file',
+        maxFileSize: 3000000, // 3 Megs 
+        onDragEnter: function(){
+          this.addClass('active');
+        },
+        onDragLeave: function(){
+          this.removeClass('active');
+        },
+        onBeforeUpload: function(id){
+          console.log('Starting the upload of #' + id);
+        },
+        onUploadProgress: function(id, percent){
+          console.log('progress' + id + ":" + percent);
+        },
+        onUploadSuccess: function(id, data){
+          console.log('Server Response for file #' + id + ': ' + JSON.stringify(data));
+          console.log('Upload of file #' + id + ' COMPLETED', 'success');
+          $('#back_idcard img').attr('src','http://82.165.190.74/images/'+data.name);
+          $('#back_idcard img').show();
+        },
+        onUploadError: function(id, xhr, status, message){
+          console.log('danger' + id + ":" + status+ "-" + message);
+        }
+      });
+      $('#selfie').dmUploader({ //
+        url: 'https://82.165.190.74:3001/api/uploadID',
+        fileName:'file',
+        maxFileSize: 3000000, // 3 Megs 
+        onDragEnter: function(){
+          this.addClass('active');
+        },
+        onDragLeave: function(){
+          this.removeClass('active');
+        },
+        onBeforeUpload: function(id){
+          console.log('Starting the upload of #' + id);
+        },
+        onUploadProgress: function(id, percent){
+          console.log('progress' + id + ":" + percent);
+        },
+        onUploadSuccess: function(id, data){
+          console.log('Server Response for file #' + id + ': ' + JSON.stringify(data));
+          console.log('Upload of file #' + id + ' COMPLETED', 'success');
+          $('#selfie img').attr('src','http://82.165.190.74/images/'+data.name);
+          $('#selfie img').show();
         },
         onUploadError: function(id, xhr, status, message){
           console.log('danger' + id + ":" + status+ "-" + message);
